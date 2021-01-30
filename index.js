cfe9957cbf389e5b8cd32e7b98c050988865243e
Raveen K Sharma <raveen08@gmail.com>
2021-01-30 21:00:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function testFunc() {
 
 function superbowlWin(record){
 let result = record.find( record => record.result === "W" )
+//looking to see if any 
 return !!result ? result.year : undefined
 }
